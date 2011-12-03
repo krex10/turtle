f5fff578639137556d6d66824c89700150cce0d7
Kavi K <kavi_nessen@hotmail.com>
2011-12-02 23:30:33 -0500

    stuff done at work

--- more_info.js
+++ more_info.js
@@ -1,12 +1,18 @@
-function more_info (rentalid ,address, cost, distance, available, utils_included, lease_required, furnished, empty_rooms)
-{
-    var class_id = "."+rentalid;
-    var all_info = "Available: "+available
-                    +"<br/>Utilities included: "+utils_included+"<br/>Lease required: "+lease_required
-                    +"<br/>Furnished: "+furnished+"<br/>Rooms: "+empty_rooms;
-    $(class_id).qtip({
-        content: all_info,
+function more_info (list_info) {
+    $("#search_results").on("mouseover", tester(list_info));
+}
+
+function tester (list_info) {
+    var list_length = list_info.length;
+    var info;
+    for (var i=0; i < list_length; i++) {
+        var selector_id = "."+i;
+        info = "Price: $ "+list_info[i][2]+"</br>Distance: "+list_info[i][3]+" km<br/>Available: "+list_info[i][4]
+            +"<br/>Utilities: "+list_info[i][5]+"<br/>Lease: "+list_info[i][6]+"<br/>Furnished: "+list_info[i][7]
+            +"<br/>Rooms: "+list_info[i][8];
+        $(selector_id).qtip({
+        content: info,
         show: 'mouseover',
-        hide: 'mouseout',   
-    })
+        hide: 'mouseout', });
+    }
 }
