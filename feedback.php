5bdbf8b71df1a55277ab0ace1435d1480520ec60
Kavi Kistnasamy <kavi@freshbooks.com>
2011-12-16 15:50:30 -0500

    safe stuff

---
+++ feedback.php
@@ -0,0 +1,18 @@
+<?php
+session_start(); 
+include "config.php";
+if (!isset($_SESSION)) { 
+	echo "{\"result\": \"Session not set, please verify cookie settings and reload the page\"}"; 
+	exit;
+}
+$var = @$_GET['f'];
+$trimmed = trim($var);
+$supertrimmed = mysql_escape_string($trimmed);
+if (mail("kavi_nessen@hotmail.com", "Turtle Contact", $supertrimmed))
+{
+	echo "{\"debug\": \"worked\"}";
+}
+else {
+	echo "{\"debug\": \"not worked\"}";
+}
+?>
